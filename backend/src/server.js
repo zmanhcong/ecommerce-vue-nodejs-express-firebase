ae8eae99543a50d655f454bd68c55c32ab56709c
nmcong1 <nmcong1>
2023-12-29 11:32:43 +0900

    Add product detail endpoint and cart functionality

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import path from "path";
 const app = express();
 app.use(bodyParser.json());
 
+// Get all products
 app.get("/api/products", async (req, res) => {
     try {
         const client = await MongoClient.connect("mongodb://localhost:27017", {
@@ -20,6 +21,26 @@ app.get("/api/products", async (req, res) => {
     }
 });
 
+// View detail of a product
+app.get("/api/products/:productId", async (req, res) => {
+    try {
+        const { productId } = req.params;
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+        });
+        const db = client.db("vue-db");
+        const product = await db.collection("products").findOne({ id: productId });
+        if (!product) {
+            res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json(product);
+        client.close();
+    } catch (err) {
+        res.status(500).json({ message: "Error connecting to db", err });
+    }
+});
+
+// Get all cart items for a user
 app.get("/api/users/:userId/cart", async (req, res) => {
     try {
         const { userId } = req.params;
@@ -45,18 +66,60 @@ app.get("/api/users/:userId/cart", async (req, res) => {
     }
 });
 
-app.get("/api/products/:productId", async (req, res) => {
+// Add a product to cart
+app.post("/api/users/:userId/cart", async (req, res) => {
     try {
-        const { productId } = req.params;
+        const { userId } = req.params;
+        const { productId } = req.body;
+        console.log("🚀 log of productId:", productId);
+
         const client = await MongoClient.connect("mongodb://localhost:27017", {
             useNewUrlParser: true,
+            useUnifiedTopology: true,
         });
         const db = client.db("vue-db");
+        const user = await db.collection("users").findOne({ id: userId });
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+        }
         const product = await db.collection("products").findOne({ id: productId });
         if (!product) {
             res.status(404).json({ message: "Product not found" });
         }
-        res.status(200).json(product);
+        await db
+            .collection("users")
+            .updateOne({ id: userId }, { $push: { cartItems: productId } });
+        res.status(200).json({ message: "Product added to cart" });
+        client.close();
+    } catch (err) {
+        res.status(500).json({ message: "Error connecting to db", err });
+    }
+});
+
+// Remove a product from cart
+app.delete("/api/users/:userId/cart/:productId", async (req, res) => {
+    try {
+        const { userId, productId } = req.params;
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = client.db("vue-db");
+
+        // find user in users collection
+        const user = await db.collection("users").findOne({ id: userId });
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+        }
+        // find product in products collection
+        const product = await db.collection("products").findOne({ id: productId });
+        if (!product) {
+            res.status(404).json({ message: "Product not found" });
+        }
+        await db
+            .collection("users")
+            .updateOne({ id: userId }, { $pull: { cartItems: productId } });
+        res.status(200).json({ message: "Product removed from cart" });
         client.close();
     } catch (err) {
         res.status(500).json({ message: "Error connecting to db", err });
