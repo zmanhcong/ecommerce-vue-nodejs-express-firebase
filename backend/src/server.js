e409403000179cd7f4adb0acd95e71a4d4445e31
nmcong1 <nmcong1>
2023-12-29 10:20:49 +0900

    add api nodejs

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,65 @@ import { MongoClient } from "mongodb";
 import path from "path";
 
 const app = express();
-
 app.use(bodyParser.json());
 
-app.listen(8000, () => {
-    console.log("Serve is listening on port 8080");
+app.get("/api/products", async (req, res) => {
+    try {
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+        });
+        const db = client.db("vue-db");
+        const products = await db.collection("products").find({}).toArray();
+        client.close();
+        res.status(200).json(products);
+    } catch (err) {
+        res.status(500).json({ message: "Error connecting to db", err });
+    }
+});
+
+app.get("/api/users/:userId/cart", async (req, res) => {
+    try {
+        const { userId } = req.params;
+        console.log(userId);
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        const db = client.db("vue-db");
+        const user = await db.collection("users").findOne({ id: userId });
+        if (!user) {
+            res.status(404).json({ message: "User not found" });
+        }
+        const products = await db.collection("products").find({}).toArray();
+        const cartItemsIds = user.cartItems;
+        const cartItems = cartItemsIds.map((id) =>
+            products.find((product) => product.id === id)
+        );
+        res.status(200).json(cartItems);
+        client.close();
+    } catch (err) {
+        res.status(500).json({ message: "Error connecting to db", err });
+    }
+});
+
+app.get("/api/products/:productId", async (req, res) => {
+    try {
+        const { productId } = req.params;
+        const client = await MongoClient.connect("mongodb://localhost:27017", {
+            useNewUrlParser: true,
+        });
+        const db = client.db("vue-db");
+        const product = await db.collection("products").findOne({ id: productId });
+        if (!product) {
+            res.status(404).json({ message: "Product not found" });
+        }
+        res.status(200).json(product);
+        client.close();
+    } catch (err) {
+        res.status(500).json({ message: "Error connecting to db", err });
+    }
+});
+
+app.listen(8001, () => {
+    console.log("Server is running on port 8001");
 });
